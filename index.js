a6f1ece7bad3c55dcba957784d6a4f79c7d6b5fb
iUnstable0 <iunstable0@gmail.com>
2024-02-10 20:11:32 -0800

    Co-authored-by: Fumi Okutsu <Easternspork@users.noreply.github.com> Co-authored-by: reedherlihy <reedherlihy@users.noreply.github.com> Co-authored-by: IvanJehad <IvanJehad@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 const express = require("express");
 
 const app = express();
-const upload = require('./upload');
+const upload = require("./upload");
 
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 
-const gm = require('gm');
-const fs = require('fs');
+const gm = require("gm");
+const fs = require("fs");
 const imageMagick = gm.subClass({ imageMagick: "7+" });
 
-const crypto = require("crypto")
-
+const crypto = require("crypto");
 
 app.use(express.json());
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
+	// Handle the uploaded file
+	res.json({ message: "File uploaded successfully!" });
 
-  // Handle the uploaded file
-  res.json({ message: 'File uploaded successfully!' });
+	console.log(
+		`convert ${req.file.path} -polaroid 0 ${
+			req.file.path.split(".")[0]
+		}-processed.png`
+	);
 
-  spawn(`convert ${req.file.path} -polaroid 0 ${req.file.path.split(".")[0]}-processed.png`)
+	spawn(`convert`, [
+		req.file.path,
+		"-resize",
+		"250x250",
+		"-gravity",
+		"center",
+		"-background",
+		"white",
+		"-extent",
+		"250x250",
+		"-polaroid",
+		"0",
+		`${req.file.path.split(".")[0]}-processed.png`,
+	]);
 
-  // imageMagick()
-  // .command("convert")
-  // // .in("-caption",  "mycaption")
-  // .in('"' + req.file.path + '"')
-  // // .in("-thumbnail",  "250x250")
-  // .in("polaroid", "0")
-  //  // insert other options...
-  // .write(`"${req.file.path.split(".")[0]}_processed.png"`, function (err) {
-  //    if (err) return console.log(err);
-  // });
-  
+	// imageMagick()
+	// .command("convert")
+	// // .in("-caption",  "mycaption")
+	// .in('"' + req.file.path + '"')
+	// // .in("-thumbnail",  "250x250")
+	// .in("polaroid", "0")
+	//  // insert other options...
+	// .write(`"${req.file.path.split(".")[0]}_processed.png"`, function (err) {
+	//    if (err) return console.log(err);
+	// });
 });
 
 app.get("/", (req, res) => {
